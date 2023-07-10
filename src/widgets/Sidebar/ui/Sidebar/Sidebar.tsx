6cdb3775e82ec71b3745cf793534741c91ddf56b
bazyl <bazylkhan96@gmail.com>
2023-07-11 01:20:54 +0600

    Добавлена i18n интернационализация. Установлены плагины Define plugin и i18n support. Переводы для каждой страницы подгружаются чанками

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss"
 import {useState} from "react";
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "shared/ui/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
     className?: string;
@@ -19,7 +20,7 @@ export const Sidebar = ({className}: SidebarProps) => {
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                {/*langSwitcher*/}
+                <LangSwitcher className={cls.lang} />
             </div>
         </div>
     );
