d32c5bfbf08dd9610976792bbb4ce865d96e5cee
bazyl <bazylkhan96@gmail.com>
2023-11-22 22:16:07 +0600

    Тест не работал с абсолютными импортами

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -4,16 +4,6 @@
  */
 
 export default {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\bazyl\\AppData\\Local\\Temp\\jest",
-
-    // Automatically clear mock calls, instances and results before every test
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
@@ -27,6 +17,9 @@ export default {
     ],
     moduleDirectories: [
         'node_modules',
+    ],
+    modulePaths: [
+        '<rootDir>src',
     ],
     testMatch: [
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
