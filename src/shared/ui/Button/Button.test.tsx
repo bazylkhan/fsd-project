6d94886cb443cb02fdf70b1b920e506a31cadb64
bazyl <bazylkhan96@gmail.com>
2024-03-08 23:32:12 +0600

    Обновление сайдбара, обновление кнопки, исправление тестов

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 describe('Button', () => {
     test('inner text', () => {
@@ -7,7 +7,7 @@ describe('Button', () => {
         expect(screen.getByText('Test')).toBeInTheDocument();
     });
     test('theme clear', () => {
-        render(<Button theme={ThemeButton.CLEAR}>Test</Button>);
+        render(<Button theme={ButtonTheme.CLEAR}>Test</Button>);
         expect(screen.getByText('Test')).toHaveClass('clear');
         screen.debug();
     });
